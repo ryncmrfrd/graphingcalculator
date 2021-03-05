82b1b4954bb33ba85cbaaa0f29ac50b775ff8566
Ryan Comerford <lordfennec@gmail.com>
2021-03-05 15:08:29 +1100

    Original project (prototype)

---
+++ Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(518, 461)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(518, 487)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Graph"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(10, 10)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(500, 500)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(515, 445)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Equation:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(624, 521)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Graphing Test"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            painGraphFromExpression(e, PictureBox1.Size, -250, 250, -250, 250)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Invalidate()
+    End Sub
+
+    Public Sub painGraphFromExpression(
+        ByVal painObject As PaintEventArgs,
+        ByVal boxControlSize As Size,
+        ByVal xLowerBound As Integer,
+        ByVal xHigherBound As Integer,
+        ByVal yLowerBound As Integer,
+        ByVal yHigherBound As Integer
+    )
+        Dim xDifference As Integer = xHigherBound - xLowerBound
+        Dim yDifference As Integer = yHigherBound - yLowerBound
+
+        Dim arrGraphPoints(xDifference) As Point,
+            bluePen As Pen = New Pen(Color.Red, 1)
+
+        Dim i As Integer = 0
+        For x = xLowerBound To xHigherBound
+
+            Dim y As Integer = tryCalcYValue(TextBox1.Text, x)
+
+            Dim xAdj As Integer = (xDifference / 2) - ((x / xDifference) * boxControlSize.Width),
+                yAdj As Integer = (yDifference / 2) - ((y / yDifference) * boxControlSize.Height)
+
+            arrGraphPoints(i) = New Point(xAdj, yAdj)
+
+            i += 1
+        Next
+
+        painObject.Graphics.DrawCurve(bluePen, arrGraphPoints)
+    End Sub
+
+    Public Function tryCalcYValue(
+        ByVal fn As String,
+        ByVal x As Integer
+    )
+        Dim y As Integer
+
+        Try
+            y = New DataTable().Compute(fn.ToLower().Replace("x", Str(-x)), Nothing) ' graphic function
+        Catch ex As Exception
+            MsgBox("Invalid function. Close application")
+            Return False
+        End Try
+
+        Return y
+    End Function
+
+End Class
