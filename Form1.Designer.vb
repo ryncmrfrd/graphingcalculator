173c1130e21a48baf0d286796165db59b1cfa8a3
Ryan Comerford <lordfennec@gmail.com>
2021-03-06 16:09:25 +1100

    add bounds + antialiasing to ui

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class graphForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,67 +22,128 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.txtEquation = New System.Windows.Forms.TextBox()
+        Me.btnGraph = New System.Windows.Forms.Button()
+        Me.pbxGraph = New System.Windows.Forms.PictureBox()
+        Me.lblEquation = New System.Windows.Forms.Label()
+        Me.lblLowerBound = New System.Windows.Forms.Label()
+        Me.txtLowerBound = New System.Windows.Forms.TextBox()
+        Me.lblUpperBound = New System.Windows.Forms.Label()
+        Me.txtUpperBound = New System.Windows.Forms.TextBox()
+        Me.chkAntialiasing = New System.Windows.Forms.CheckBox()
+        CType(Me.pbxGraph, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtEquation
         '
-        Me.TextBox1.Location = New System.Drawing.Point(518, 461)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.txtEquation.Location = New System.Drawing.Point(518, 445)
+        Me.txtEquation.Name = "txtEquation"
+        Me.txtEquation.Size = New System.Drawing.Size(100, 20)
+        Me.txtEquation.TabIndex = 5
         '
-        'Button1
+        'btnGraph
         '
-        Me.Button1.Location = New System.Drawing.Point(518, 487)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Graph"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnGraph.Location = New System.Drawing.Point(518, 487)
+        Me.btnGraph.Name = "btnGraph"
+        Me.btnGraph.Size = New System.Drawing.Size(100, 23)
+        Me.btnGraph.TabIndex = 7
+        Me.btnGraph.Text = "Graph"
+        Me.btnGraph.UseVisualStyleBackColor = True
         '
-        'PictureBox1
+        'pbxGraph
         '
-        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox1.Location = New System.Drawing.Point(10, 10)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(1)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(500, 500)
-        Me.PictureBox1.TabIndex = 3
-        Me.PictureBox1.TabStop = False
+        Me.pbxGraph.Location = New System.Drawing.Point(10, 10)
+        Me.pbxGraph.Margin = New System.Windows.Forms.Padding(0)
+        Me.pbxGraph.Name = "pbxGraph"
+        Me.pbxGraph.Size = New System.Drawing.Size(500, 500)
+        Me.pbxGraph.TabIndex = 3
+        Me.pbxGraph.TabStop = False
         '
-        'Label1
+        'lblEquation
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(515, 445)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(52, 13)
-        Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Equation:"
+        Me.lblEquation.AutoSize = True
+        Me.lblEquation.Location = New System.Drawing.Point(515, 429)
+        Me.lblEquation.Name = "lblEquation"
+        Me.lblEquation.Size = New System.Drawing.Size(52, 13)
+        Me.lblEquation.TabIndex = 4
+        Me.lblEquation.Text = "Equation:"
         '
-        'Form1
+        'lblLowerBound
         '
+        Me.lblLowerBound.AutoSize = True
+        Me.lblLowerBound.Location = New System.Drawing.Point(515, 9)
+        Me.lblLowerBound.Name = "lblLowerBound"
+        Me.lblLowerBound.Size = New System.Drawing.Size(73, 13)
+        Me.lblLowerBound.TabIndex = 0
+        Me.lblLowerBound.Text = "Lower Bound:"
+        '
+        'txtLowerBound
+        '
+        Me.txtLowerBound.Location = New System.Drawing.Point(518, 25)
+        Me.txtLowerBound.Name = "txtLowerBound"
+        Me.txtLowerBound.Size = New System.Drawing.Size(100, 20)
+        Me.txtLowerBound.TabIndex = 1
+        Me.txtLowerBound.Text = "-25"
+        '
+        'lblUpperBound
+        '
+        Me.lblUpperBound.AutoSize = True
+        Me.lblUpperBound.Location = New System.Drawing.Point(515, 48)
+        Me.lblUpperBound.Name = "lblUpperBound"
+        Me.lblUpperBound.Size = New System.Drawing.Size(73, 13)
+        Me.lblUpperBound.TabIndex = 2
+        Me.lblUpperBound.Text = "Upper Bound:"
+        '
+        'txtUpperBound
+        '
+        Me.txtUpperBound.Location = New System.Drawing.Point(518, 64)
+        Me.txtUpperBound.Name = "txtUpperBound"
+        Me.txtUpperBound.Size = New System.Drawing.Size(100, 20)
+        Me.txtUpperBound.TabIndex = 3
+        Me.txtUpperBound.Text = "25"
+        '
+        'chkAntialiasing
+        '
+        Me.chkAntialiasing.AutoSize = True
+        Me.chkAntialiasing.Checked = True
+        Me.chkAntialiasing.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkAntialiasing.Location = New System.Drawing.Point(518, 468)
+        Me.chkAntialiasing.Name = "chkAntialiasing"
+        Me.chkAntialiasing.Size = New System.Drawing.Size(85, 17)
+        Me.chkAntialiasing.TabIndex = 6
+        Me.chkAntialiasing.Text = "Antialiasing?"
+        Me.chkAntialiasing.UseVisualStyleBackColor = True
+        '
+        'graphForm
+        '
+        Me.AcceptButton = Me.btnGraph
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(624, 521)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Name = "Form1"
-        Me.Text = "Graphing Test"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Controls.Add(Me.chkAntialiasing)
+        Me.Controls.Add(Me.lblUpperBound)
+        Me.Controls.Add(Me.txtUpperBound)
+        Me.Controls.Add(Me.lblLowerBound)
+        Me.Controls.Add(Me.txtLowerBound)
+        Me.Controls.Add(Me.lblEquation)
+        Me.Controls.Add(Me.pbxGraph)
+        Me.Controls.Add(Me.btnGraph)
+        Me.Controls.Add(Me.txtEquation)
+        Me.Name = "graphForm"
+        Me.Text = "Graph Equation"
+        CType(Me.pbxGraph, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents txtEquation As TextBox
+    Friend WithEvents btnGraph As Button
+    Friend WithEvents pbxGraph As PictureBox
+    Friend WithEvents lblEquation As Label
+    Friend WithEvents lblLowerBound As Label
+    Friend WithEvents txtLowerBound As TextBox
+    Friend WithEvents lblUpperBound As Label
+    Friend WithEvents txtUpperBound As TextBox
+    Friend WithEvents chkAntialiasing As CheckBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,16 +1,33 @@
-﻿Public Class Form1
+﻿Public Class graphForm
 
-    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
-        If Not String.IsNullOrEmpty(TextBox1.Text) Then
-            painGraphFromExpression(e, PictureBox1.Size, -250, 250, -250, 250)
+    Dim brdrPen As Pen = New Pen(Color.Gray, 1),
+        axisPen As Pen = New Pen(Color.Red, 1),
+        gridPen As Pen = New Pen(Color.LightGray, 1),
+        mainPen As Pen = New Pen(Color.Blue, 1)
+
+    Private Sub pbxGraph_Paint(sender As Object, e As PaintEventArgs) Handles pbxGraph.Paint
+        If Not String.IsNullOrEmpty(txtEquation.Text) Then
+            e.Graphics.SmoothingMode = If(chkAntialiasing.Checked, Drawing2D.SmoothingMode.AntiAlias, Drawing2D.SmoothingMode.None)
+
+            Dim xSetLowerBound As Integer = Integer.Parse(txtLowerBound.Text),
+                xSetUpperBound As Integer = Integer.Parse(txtUpperBound.Text)
+
+            paintAxisGridLines(e, pbxGraph.Size, xSetLowerBound, xSetUpperBound, xSetLowerBound, xSetUpperBound)
+            painGraphFromExpression(txtEquation.Text, e, pbxGraph.Size, xSetLowerBound, xSetUpperBound, xSetLowerBound, xSetUpperBound)
         End If
+
+        e.Graphics.DrawRectangle(brdrPen, 0, 0, pbxGraph.Width - 1, pbxGraph.Height - 1)
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        PictureBox1.Invalidate()
+    Private Sub txtLowerBound_TextChanged(sender As Object, e As EventArgs) Handles txtLowerBound.TextChanged
+        txtUpperBound.Text = Math.Abs(Integer.Parse(txtLowerBound.Text))
     End Sub
 
-    Public Sub painGraphFromExpression(
+    Private Sub btnGraph_Click(sender As Object, e As EventArgs) Handles btnGraph.Click
+        pbxGraph.Invalidate()
+    End Sub
+
+    Public Sub paintAxisGridLines(
         ByVal painObject As PaintEventArgs,
         ByVal boxControlSize As Size,
         ByVal xLowerBound As Integer,
@@ -21,24 +38,59 @@
         Dim xDifference As Integer = xHigherBound - xLowerBound
         Dim yDifference As Integer = yHigherBound - yLowerBound
 
-        Dim arrGraphPoints(xDifference) As Point,
-            bluePen As Pen = New Pen(Color.Red, 1)
+        For x = xLowerBound To xHigherBound
+            Dim xAdj As Integer = adjustPointValue(x, xDifference, boxControlSize.Width)
+            painObject.Graphics.DrawLine(If(x = 0, Me.axisPen, Me.gridPen), xAdj, 0, xAdj, boxControlSize.Height)
+        Next
+
+        For y = yLowerBound To yHigherBound
+            Dim yAdj As Integer = adjustPointValue(y, yDifference, boxControlSize.Height)
+            painObject.Graphics.DrawLine(If(y = 0, Me.axisPen, Me.gridPen), 0, yAdj, boxControlSize.Width, yAdj)
+        Next
+
+        ' AXIS LINES
+        painObject.Graphics.DrawLine(Me.axisPen, adjustPointValue(0, xDifference, boxControlSize.Width), 0, adjustPointValue(0, xDifference, boxControlSize.Width), boxControlSize.Height)
+        painObject.Graphics.DrawLine(Me.axisPen, 0, adjustPointValue(0, yDifference, boxControlSize.Height), boxControlSize.Width, adjustPointValue(0, yDifference, boxControlSize.Height))
+    End Sub
+
+    Public Sub painGraphFromExpression(
+        ByVal expression As String,
+        ByVal painObject As PaintEventArgs,
+        ByVal boxControlSize As Size,
+        ByVal xLowerBound As Integer,
+        ByVal xHigherBound As Integer,
+        ByVal yLowerBound As Integer,
+        ByVal yHigherBound As Integer
+    )
+        Dim xDifference As Integer = xHigherBound - xLowerBound
+        Dim yDifference As Integer = yHigherBound - yLowerBound
+
+        Dim arrGraphPoints(xDifference) As Point
 
         Dim i As Integer = 0
         For x = xLowerBound To xHigherBound
 
-            Dim y As Integer = tryCalcYValue(TextBox1.Text, x)
+            Dim y As Integer = tryCalcYValue(expression, x)
 
-            Dim xAdj As Integer = (xDifference / 2) - ((x / xDifference) * boxControlSize.Width),
-                yAdj As Integer = (yDifference / 2) - ((y / yDifference) * boxControlSize.Height)
+            Dim xAdj As Integer = adjustPointValue(x, xDifference, boxControlSize.Width),
+                yAdj As Integer = adjustPointValue(y, yDifference, boxControlSize.Height)
 
             arrGraphPoints(i) = New Point(xAdj, yAdj)
 
             i += 1
         Next
 
-        painObject.Graphics.DrawCurve(bluePen, arrGraphPoints)
+        painObject.Graphics.DrawCurve(Me.mainPen, arrGraphPoints)
     End Sub
+
+    Public Function adjustPointValue(
+        ByVal val As Integer,
+        ByVal axisDifference As Integer,
+        ByVal controlAxisSize As Integer
+    )
+        Dim res As Integer = (controlAxisSize / 2) - ((val / axisDifference) * controlAxisSize)
+        Return res
+    End Function
 
     Public Function tryCalcYValue(
         ByVal fn As String,
@@ -47,9 +99,10 @@
         Dim y As Integer
 
         Try
-            y = New DataTable().Compute(fn.ToLower().Replace("x", Str(-x)), Nothing) ' graphic function
+            y = New DataTable().Compute(fn.ToLower().Replace("x", Str(-x)), Nothing)
         Catch ex As Exception
-            MsgBox("Invalid function. Close application")
+            MsgBox("Invalid function. TEMP action: Closing application") ' TEMP FIXME please
+            End
             Return False
         End Try
 
